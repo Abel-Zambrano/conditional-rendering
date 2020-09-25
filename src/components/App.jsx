a89141584b06155a0615aa88c62cb3a42146a6ea
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-09-24 18:28:30 -0700

    added conditions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Login from "./Login";
 
-const isLoggedIn = true;
-const time = new Date().getHours();
+const isRegistered = false;
+
 
 // Ternerary Operator
 function App() {
   return (
     <div className="container">
-      {isLoggedIn ? <h1>Hello</h1> : <Login />}
-      {time > 12 && <h1>Good Afternoon!</h1>}
+      <Login registered={isRegistered} />
     </div>
   );
 }
+
+// const time = new Date().getHours();
+// {time > 12 && <h1>Good Afternoon!</h1>}
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Input from './Input';
 
-function Login() {
+function Login(props) {
     return (
         <form className="form">
             <Input type="text" placeholder="Username" />
             <Input type="password" placeholder="Password" />
-            <button type="submit">Login</button>
+            {props.registered === false && <Input type="password" placeholder="Confirm Password" />}
+            <button type="submit">{props.registered ? "Login" : "Register"}</button>
         </form>
     );
 }
